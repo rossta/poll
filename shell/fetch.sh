aacb78e81ea02e60fb912753603e354c757e5cdc
Ross Kaffenberger <ross@weplay.com>
2008-10-08 11:49:13 -0400

    New polls; dynamic bg styles

---
+++ shell/fetch.sh
@@ -0,0 +1,1 @@
+curl http://www.electoral-vote.com/evp2008/Pres/Excel/today.csv | cut -d, -f 1,2,3,4,5,6 | grep -e "\(S[tu][am][ts]\)" -v | tee "archive/polls-$(date +%F).csv" | tee latest_polls.csv
